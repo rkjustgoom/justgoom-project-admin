1a7f19ee1a4768265e6b6035017b0ada716cae07
kanupatel <rk@justgoom.com>
2026-06-19 13:33:06 +0530

    commit

--- admin/project/resources/views/front/partials/footer-mini.blade.php
+++
@@ -1,5 +0,0 @@
-<footer class="auth-mini-footer">
-  <div class="container auth-mini-footer-inner">
-    <span>© 2026 Just Goom LLP</span>
-  </div>
-</footer>
--- admin/project/resources/views/front/partials/users/footer.blade.php
+++
@@ -1,1 +0,0 @@
-<footer class="user-footer">© 2026 Just Goom LLP. Trademarks and brands are the property of their respective owners.</footer>
